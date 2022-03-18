be1a18353e35ff73f21332ce90ecb45dd4b06e66
VladimirNedoroslev <v-v.nedoroslev@lionbridge.com>
2022-03-18 18:18:51 +0600

    4. Add services done;

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {HEROES} from "../mock-heroes";
+import {HeroService} from "../hero.service";
 import {Hero} from "../hero";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +10,25 @@ import {Hero} from "../hero";
 })
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
-  heroes: Hero[] = HEROES;
+  heroes: Hero[] = [];
 
-  constructor() {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+
 }
