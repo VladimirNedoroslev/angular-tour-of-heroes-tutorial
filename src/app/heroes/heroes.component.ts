0334364b00a77fe8eb914a0c682fa309ce5f32d0
VladimirNedoroslev <v-v.nedoroslev@lionbridge.com>
2022-03-21 12:55:17 +0600

    4. Add navigation done;

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HeroService} from "../hero.service";
 import {Hero} from "../hero";
-import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -12,23 +11,15 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-
 }
